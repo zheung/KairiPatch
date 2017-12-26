f11c304f898a7ac0f480631197c10ada605745be
DanoR <zheung@vip.qq.com>
2017-12-26 13:01:27 +0800

    1712261

---
+++ work/dll/_upac.bat
@@ -0,0 +1,2 @@
+copy Assembly-CSharp.dll assets\bin\Data\Managed\Assembly-CSharp.dll
+"C:\Program Files\2345Soft\HaoZip\HaoZipC" a -tzip ma.apk assets
---
+++ work/u3d/en-cont.bat
@@ -0,0 +1,8 @@
+@echo off
+
+del container.dat
+del container.uni.crack
+D:\Krsma\tool\ma43\MAencode43.exe container.unity3d
+ren container.uni.crack container.dat
+
+pause
