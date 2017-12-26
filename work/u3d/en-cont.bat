647e3a84ef973c10ad066c2bdf5adbe1023cf4b8
DanoR <zheung@vip.qq.com>
2017-12-27 00:01:17 +0800

    1712271

--- work/u3d/en-cont.bat
+++ work/u3d/en-cont.bat
@@ -1,8 +1,5 @@
 @echo off
 
 del container.dat
-del container.uni.crack
 D:\Krsma\tool\ma43\MAencode43.exe container.unity3d
 ren container.uni.crack container.dat
-
-pause
---
+++ work/ver/en-ver.bat
@@ -0,0 +1,5 @@
+@echo off
+
+del version.dat
+D:\Krsma\tool\ma43\MAencode43.exe version.unity3d
+ren version.uni.crack version.dat
