46a276cf4cb3679b97c0dca48c0fc0c1acf1e6cf
DanoR <zheung@vip.qq.com>
2018-07-08 04:06:33 +0800

    add jp dll

--- work/en-apk-home.bat
+++ work/en-apk-home.bat
@@ -1,5 +1,5 @@
 set /p type=input mod type:
-cd dll
+cd dlljp
 copy Assembly-CSharp.%type%.dll assets\bin\Data\Managed\Assembly-CSharp.dll
 "E:\Program\HaoZip\HaoZipC" a -tzip ..\ma.apk assets
-copy ..\ma.apk D:\File\Nox_share\Other\krsma.cn.602.%type%.apk
+copy ..\ma.apk D:\File\Nox_share\Other\krsma.jp.602.%type%.apk
--- work/en-apk-work.bat
+++ work/en-apk-work.bat
@@ -1,5 +1,5 @@
 set /p type=input mod type:
-cd dll
+cd dlljp
 copy Assembly-CSharp.%type%.dll assets\bin\Data\Managed\Assembly-CSharp.dll
 "C:\Program Files\2345Soft\HaoZip\HaoZipC" a -tzip ..\ma.apk assets
-copy ..\ma.apk C:\Users\Administrator\Nox_share\Other\krsma.cn.602.%type%.apk
+copy ..\ma.apk C:\Users\Administrator\Nox_share\Other\krsma.jp.602.%type%.apk
